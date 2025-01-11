e251f8c114afa9044e395f470cfb63c36ea56614
mrsvolodya <vovamursky@gmail.com>
2025-01-11 22:27:37 +0200

    implement model in order to describe structure of data

---
+++ Models/TodoItem.cs
@@ -0,0 +1,10 @@
+namespace TodoApi.Models
+{
+  public class TodoItem
+  {
+    public int Id { get; set; }
+    public required string Title { get; set; }
+
+    public bool IsCompleted { get; set; }
+  }
+}
